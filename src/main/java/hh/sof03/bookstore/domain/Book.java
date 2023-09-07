0fa5def5a67daa9d1a0d7440bdde50410a16bda3
Minea <117983209+mineaaa@users.noreply.github.com>
2023-09-07 10:52:21 +0300

    ekat projektilisäykset

---
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -0,0 +1,11 @@
+package hh.sof03.bookstore.domain;
+
+public class Book {
+
+    private String author;
+    private String title;
+    private int year;
+    private String isbn;
+    private double price;
+
+}
---
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -0,0 +1,9 @@
+package hh.sof03.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class BookController {
+
+}
