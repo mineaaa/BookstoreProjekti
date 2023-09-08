ecf87f223483c0f063c32980e6a363a801a6e8b5
Minea <117983209+mineaaa@users.noreply.github.com>
2023-09-08 15:22:43 +0300

    Lisäsin controller luokkaan uusia juttuja + timelist + getter setter konstruktor wombo combo

--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -7,6 +7,67 @@ public class Book {
     private int year;
     private String isbn;
     private double price;
-    private double test;
+
+    public Book() {
+        this.author = null;
+        this.title = null;
+        this.year = 0;
+        this.isbn = null;
+        this.price = 0;
+    }
+
+    public Book(String author, String title, int year, String isbn, double price) {
+        this.author = author;
+        this.title = title;
+        this.year = year;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [author=" + author + ", title=" + title + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+                + "]";
+    }
 
 }
