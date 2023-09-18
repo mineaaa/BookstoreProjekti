52039c8cc88b4cc58b54a793a15654dd86a0c6ed
Minea <117983209+mineaaa@users.noreply.github.com>
2023-09-18 14:15:18 +0300

    controller luokkaan muutos

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,18 +1,34 @@
 package hh.sof03.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import hh.sof03.bookstore.domain.BookRepository;
 
 @Controller
 
 public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
 
     @RequestMapping(value = "index", method = RequestMethod.GET)
 
     public String mainPage() {
 
         return "bookThymeleaf";
+    }
+
+    @RequestMapping(value = "booklist", method = RequestMethod.GET)
+    public String listAllBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "booklist";
+
     }
 
 }
