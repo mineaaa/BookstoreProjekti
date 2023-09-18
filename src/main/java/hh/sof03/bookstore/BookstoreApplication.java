ffdb7f69b10b8e688ad708f161b90295540c260e
Minea <117983209+mineaaa@users.noreply.github.com>
2023-09-18 13:08:01 +0300

    tietokannan lisääminen

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -1,13 +1,35 @@
 package hh.sof03.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.bookstore.domain.Book;
+import hh.sof03.bookstore.domain.BookRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+
+	public CommandLineRunner bookStore(BookRepository bookStoreRepository) {
+		return (args) -> {
+
+			log.info("kirjojen tallettaminen");
+			bookStoreRepository.save(new Book("Minni Hiiri", "Minni Hiiren seikkailut", 2023, "hiiri123", 13.99));
+			bookStoreRepository.save(new Book("Mikki Hiiri", "Hiiren elämä", 2022, "hiiri234", 10.50));
+
+		};
 	}
 
 }
--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -1,9 +1,22 @@
 package hh.sof03.bookstore.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
     private String author;
     private String title;
+    @Column(name = "publishing_year")
     private int year;
     private String isbn;
     private double price;
